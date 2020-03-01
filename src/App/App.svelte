ceac8c224dec9591100426532bc0d2855bb85de3
Ben Firth <bfirth93@gmail.com>
2020-03-01 14:47:19 -0700

    Add app folder

---
+++ src/App/App.svelte
@@ -0,0 +1,32 @@
+<script>
+	import NavBar from '../NavBar/NavBar';
+	import PicContainer from '../PicContainer/PicContainer';
+	import Picture from '../Picture/Picture';
+	let name;
+</script>
+
+<main>
+	<NavBar />
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
