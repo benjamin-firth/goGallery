43d92063ab5317a94ff8a411f35c55a797a57057
Ben Firth <bfirth93@gmail.com>
2020-03-02 11:05:05 -0700

    Pass props to picture component

---
+++ src/Picture/Picture.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let title;
+  export let img;
+</script>
+
+<div>
+  <h3>{title}</h3>
+	<img src={img} />
+</div>
+
+<style>
+  div {
+    margin: 30px;
+    width: 400px;
+  }
+
+  img {
+    width: 300px;
+  }
+</style>
