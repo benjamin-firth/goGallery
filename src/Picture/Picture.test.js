3ff65a5add7e4e4adffba3d69ad5ce9e26845eff
Ben Firth <bfirth93@gmail.com>
2020-03-02 18:09:10 -0700

    Take out unused imports from picture test file

--- src/Picture/Picture.test.js
+++ src/Picture/Picture.test.js
@@ -1,6 +1,6 @@
 import Picture from './Picture.svelte'
 import '@testing-library/jest-dom/extend-expect'
-import { render, fireEvent } from '@testing-library/svelte'
+import { render } from '@testing-library/svelte'
 
 test('shows proper title when rendered', () => {
   const { getByText } = render(Picture, { title: 'test title', img: 'test-img' })
