437aa60a7f176f0f9cd4d98b4c900bbfb5a203b2
Ben Firth <bfirth93@gmail.com>
2020-03-03 20:22:50 -0700

    Add loading and initial page to store

--- src/stores.js
+++ src/stores.js
@@ -2,3 +2,5 @@ import { writable } from 'svelte/store';
 
 export const displayedArt = writable([{title: '', primaryimageurl: '', century: ''}]);
 export const cultureID = writable(0);
+export const isLoading = writable(false);
+export const startApp = writable(true);
